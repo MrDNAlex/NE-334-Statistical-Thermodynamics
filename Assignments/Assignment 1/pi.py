8fd056bb2079c9cca281442575bb41274f3f5c94
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2024-10-03 17:46:55 -0400

    Q3

--- Assignments/Assignment 1/pi.py
+++ Assignments/Assignment 1/pi.py
@@ -1,17 +1,36 @@
 import numpy as np
 sample_size=10000
-# x and y are random numbers
-x=np.random.random(sample_size)
-y=np.random.random(sample_size)
-fin=open('xy.dat','w')
-fout=open('xy_out.dat','w')
-count_circle=0
-for i in range(sample_size):
-	r=x[i]**2+y[i]**2
-	if r <= 1:
-		fin.write(str(x[i])+' '+str(y[i])+' '+'\n')
-		count_circle+=1
-	else:
-		fout.write(str(x[i])+' '+str(y[i])+' '+'\n')
-fin.close()
-fout.close()
+
+def EstimatePi (sampleSize):
+    # Generate Uniform Distribution
+	x=np.random.random(sampleSize)
+	y=np.random.random(sampleSize)
+ 
+	# Determine the number of points inside the Circle
+	count_circle = 0
+	for i in range(sample_size):
+		r=x[i]**2+y[i]**2
+		if r <= 1:
+			count_circle+=1
+	
+	#Get the Probablility of a Point being inside the Circle
+	p = count_circle/sample_size
+ 
+	# Estimate Pi
+	pi_estimate = 4*p
+	
+	#Calculate the Standard Deviation for Binomial
+	std_binomial = np.sqrt(p * (1-p))
+	
+	# Calculate the Standard Error
+	standard_error = std_binomial/np.sqrt(sampleSize)
+ 
+	return [pi_estimate, standard_error]
+
+# Q3 b and c
+for i in range(15):
+    [pi_estimate, standard_error] = EstimatePi(sample_size)
+    print(f"Sample Size = {sample_size}, PI Estimate = {pi_estimate}, Standard Error : {standard_error}")
+    sample_size*=2
+
+    
